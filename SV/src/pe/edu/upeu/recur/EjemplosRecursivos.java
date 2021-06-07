23028f43ddd2c96afb1c4820d64e0289db925183
fraynilson2003 <fraynilson2003@gmail.com>
2021-06-07 11:43:31 -0500

    Recursividad

--- SV/src/pe/edu/upeu/recur/EjemplosRecursivos.java
+++ SV/src/pe/edu/upeu/recur/EjemplosRecursivos.java
@@ -1,4 +1,3 @@
-  
 package pe.edu.upeu.recur;
 
 import java.math.BigInteger;
@@ -14,8 +13,7 @@ public class EjemplosRecursivos {
         }
          return resultado;
       } 
-      
-      
+          
       public long factorialRecur(int numero) {
         long resultado=1;
         if(numero>1){
@@ -34,8 +32,7 @@ public class EjemplosRecursivos {
         }
          return resultado;
       } 
-      
-      
+            
       public BigInteger factorialRecurBig(int numero) {
         BigInteger resultado=new BigInteger("1");
         if(numero>1){
@@ -45,5 +42,58 @@ public class EjemplosRecursivos {
             return resultado;
         }        
       }    
+
+      public long fibonacci(int numero) {//5
+        long numAnt=0, numNew=1, numAux=0;
+          if(numero>1){ //  0  1:  1 2 3 5
+            for (int cont = 1; cont < numero; cont++) {
+                numAux=numNew;//1
+                numNew=numAnt+numNew;
+                numAnt=numAux;
+            }
+            return numNew;
+          }
+        return numero;
+      }
+
+      public BigInteger fibonacciBig(int numero) {//5
+        BigInteger numAnt=new BigInteger("0"), numNew=new BigInteger("1"), 
+        numAux=new BigInteger("0");
+          if(numero>1){ //  0  1:  1 2 3 5
+            for (int cont = 1; cont < numero; cont++) {
+                numAux=numNew;//1
+                numNew=numAnt.add(numNew);
+                numAnt=numAux;
+            }
+            return numNew;
+          }
+        return new BigInteger(String.valueOf(numero));
+      }   
+
+
+      public long fibonacciRecur(int numero) {
+        if(numero>1){       
+            return fibonacciRecur(numero-1)+fibonacciRecur(numero-2);
+        }
+            return numero;
+       }      
+
+       public long fibonacciX(int numero) {
+        if(numero>1){       
+            return fibonacci(numero-1)+fibonacci(numero-2);
+        }
+            return numero;
+       }   
+       
+       public BigInteger fibonacciXBig(int numero) {
+          if(numero>1){       
+              return fibonacciBig(numero-1).add(fibonacciBig(numero-2));
+          }
+            return new BigInteger(String.valueOf(numero));
+       }        
+
+
+
+
 
 }
