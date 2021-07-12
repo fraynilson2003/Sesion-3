7b893a2c2d80c54564844e3b672f886d44a37cff
fraynilson2003 <fraynilson2003@gmail.com>
2021-07-12 12:03:18 -0500

    Arreglos

---
+++ nose/Tranformados.java
@@ -0,0 +1,12 @@
+public class Tranformados {
+
+    public void contarmatrices();
+    int[][] matrizT={
+        {5,	6,	0},
+        {5,	-6,	2},
+        {0,	12,	-2},
+        {0,	6,	2}
+    };
+    
+    
+}
